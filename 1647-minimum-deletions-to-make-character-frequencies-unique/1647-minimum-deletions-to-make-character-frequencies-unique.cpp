83a71264e9cb7ec4dff4c4c04fa13404498d956e
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-28 10:25:21 +0530

    Update 1647-minimum-deletions-to-make-character-frequencies-unique.cpp

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,27 +3,12 @@ public:
     int minDeletions(string s) 
     {
         vector<int>freq(26,0);
-        // int maxx=0;
+     
         for(auto &ch:s)
         {
             freq[ch-'a']++;
-            //maxx=max(freq[ch-'a'],maxx);
         }
-        
-//         vector<int>v(maxx+1,0);
-//         for(auto &x:freq)
-//         {
-//             v[x]++;
-//         }
-        
-//         int res=0;
-//         for(int i=v.size()-1;i>0;i--)
-//         {
-//             if(!v[i])continue;
-//             res+=(v[i]-1);
-//             v[i-1]+=(v[i]-1);
-//         }
-        
+
         sort(freq.begin(),freq.end(),greater<int>());
     
         int res=0;
