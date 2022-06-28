82284051023e728e4cbfaac3512db48d36a70075
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-28 09:57:06 +0530

    Time: 83 ms (59.02%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,11 +3,11 @@ public:
     int minDeletions(string s) 
     {
         vector<int>freq(26,0);
-         int maxx=0;
+        // int maxx=0;
         for(auto &ch:s)
         {
             freq[ch-'a']++;
-            maxx=max(freq[ch-'a'],maxx);
+            //maxx=max(freq[ch-'a'],maxx);
         }
         
 //         vector<int>v(maxx+1,0);
@@ -25,9 +25,7 @@ public:
 //         }
         
         sort(freq.begin(),freq.end(),greater<int>());
-        
-        // for(int i=0;i<26;i++)
-        //     cout<<freq[i]<<" ";
+    
         int res=0;
         bool f=0;
         for(int i=0;i<25;i++)
@@ -40,14 +38,12 @@ public:
                 {
                     res+=freq[i+1];
                     freq[i+1]=0;
-                    //cout<<i<<" "<<res<<"\n";
                     continue;
                 }
                 int t=freq[i+1];
                freq[i+1]=max(freq[i]-1,0);
                 if(freq[i+1]==0)f=1;
                 res+=(t-freq[i+1]);
-                //cout<<i<<" "<<res<<"\n";
             }
         }
         
@@ -56,21 +52,3 @@ public:
     }
 };
 
-//3 2 2 1 1 1
-//3 2 1 0 0 0
-
-//2 2 2
-//2 1 
-
-//"nhgffkjjlnglnegfmcmjamdgjdf"
-//4 3 2 1 0 0 0 0 0 0 0 0
-//4 4 4 3 3 2 2 1 1 1 1 1
-//1 2 2 3 2 2 1 1 1 1 1
-
-/*
-
-"abcabc"
-"bbcebab"
-"nhgffkjjlnglnegfmcmjamdgjdf"
-
-*/
