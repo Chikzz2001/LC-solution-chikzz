225ed12b3024cf27709bb813ec2d110c25d8bf2a
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-29 21:57:57 +0530

    Time: 489 ms (71.39%), Space: 98.7 MB (83.53%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
@@ -2,30 +2,58 @@ class Solution {
 public:
     int wordCount(vector<string>& sW, vector<string>& tW) 
     {
-        unordered_map<int,unordered_set<string>>m;
+//         unordered_map<int,unordered_set<string>>m;
         
+//         for(auto &w:sW)
+//         {
+//             int n=w.length();
+//             sort(w.begin(),w.end());
+//             m[n].insert(w);
+//         }
+        
+//         int res=0;
+//         for(int i=0;i<tW.size();i++)
+//         {
+//             string s=tW[i];
+//             int n=s.length();
+//             if(!m.count(n-1))continue;
+//             sort(s.begin(),s.end());
+//             for(int i=0;i<n;i++)
+//             {
+//                 string st=s.substr(0,i)+s.substr(i+1);
+//                 if(m[n-1].count(st))
+//                 {
+//                     res++;
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+        
+        unordered_set<int>S;
         for(auto &w:sW)
         {
-            int n=w.length();
-            sort(w.begin(),w.end());
-            m[n].insert(w);
+            int x=0;
+            for(int i=0;i<w.length();i++)
+                x|=(1<<(w[i]-'a'));
+            S.insert(x);
         }
         
         int res=0;
-        for(int i=0;i<tW.size();i++)
+        for(auto &w:tW)
         {
-            string s=tW[i];
-            int n=s.length();
-            if(!m.count(n-1))continue;
-            sort(s.begin(),s.end());
-            for(int i=0;i<n;i++)
+            int x=0;
+            for(int i=0;i<w.length();i++)
+                x|=(1<<(w[i]-'a'));
+            for(auto &ch:w)
             {
-                string st=s.substr(0,i)+s.substr(i+1);
-                if(m[n-1].count(st))
+                x^=(1<<(ch-'a'));
+                if(S.count(x))
                 {
                     res++;
                     break;
                 }
+                x|=(1<<(ch-'a'));
             }
         }
         return res;
