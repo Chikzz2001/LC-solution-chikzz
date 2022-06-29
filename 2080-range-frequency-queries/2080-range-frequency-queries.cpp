b0b8470bcd93d330d6a887148040ffc65457806f
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-29 12:53:27 +0530

    Time: 945 ms (52.06%), Space: 234.8 MB (83.50%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.cpp
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -1,25 +1,10 @@
 class RangeFreqQuery {
-   // vector<vector<int>>m;
     map<int,vector<int>>freq;
-    int maxx,minn;
 public:
     RangeFreqQuery(vector<int>& arr) 
     {
-//         maxx=*max_element(arr.begin(),arr.end());
-//         minn=*min_element(arr.begin(),arr.end());
         int n=arr.size();
-//         m=vector<vector<int>>(n,vector<int>(maxx+1,0));
-        
-//         m[0][arr[0]]++;
-//         freq[arr[0]]++;
-//         for(int i=1;i<n;i++)
-//         {
-//             // for(int j=0;j<=maxx;j++)
-//             //     m[i][j]=m[i-1][j];
-//            freq[arr[i]]++;
-//            m[i][arr[i]]=freq[arr[i]];
-//         }
-        
+
         for(int i=0;i<n;i++)
             freq[arr[i]].push_back(i);
     }
@@ -36,9 +21,8 @@ public:
     }
 };
 
-/*
-
- 4:2
- idx2=1;
- 
-*/
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value);
+ */
