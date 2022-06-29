cd39a4c2cddaed4acccff5040c2dd8b23ff39bb0
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-29 12:52:45 +0530

    Time: 4 ms (78.20%), Space: 8.3 MB (84.73%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.cpp
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -4,7 +4,6 @@ public:
     {
         int n=plants.size();
  
-        //2 4 7 10
         int net=0,steps=0;
        for(int i=0;i<n;)
        {
@@ -14,11 +13,8 @@ public:
                steps++;
            }
            if(i==n)break;
-           steps+=(2*i);
-           net=0;
+           steps+=(2*i);net=0;
        }
         return steps;
     }
 };
-
-//1+1+4+1+6+1
