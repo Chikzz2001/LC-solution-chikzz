dcdf65c4f18038d79d4da278a3dd2f98ed75d0ee
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-30 10:31:28 +0530

    Time: 224 ms (78.94%), Space: 91.1 MB (92.38%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,38 +1,43 @@
 #define ll long long int
 class Solution {
-    ll dp[100001][2];
-    ll helper(vector<int>& nums,int op,int i)
-    {
-        if(i==nums.size())
-            return 0;
+//     ll dp[100001][2];
+//     ll helper(vector<int>& nums,int op,int i)
+//     {
+//         if(i==nums.size())
+//             return 0;
         
-        if(dp[i][op]!=-1)
-            return dp[i][op];
+//         if(dp[i][op]!=-1)
+//             return dp[i][op];
         
-        ll maxx=helper(nums,op,i+1);
-        if(op)
-        {
-            maxx=max(maxx,-nums[i]+helper(nums,0,i+1));
-        }
-        else
-        {
-            maxx=max(maxx,nums[i]+helper(nums,1,i+1));
-        }
-        return dp[i][op]=maxx;
-    }
+//         ll maxx=helper(nums,op,i+1);
+        
+//         if(op)
+//             maxx=max(maxx,-nums[i]+helper(nums,0,i+1));
+//         else
+//             maxx=max(maxx,nums[i]+helper(nums,1,i+1));
+        
+//         return dp[i][op]=maxx;
+//     }
 public:
     long long maxAlternatingSum(vector<int>& nums) 
     {
-        //max-min
-        /*
+        int n=nums.size();
+        //dp[i][j]-->max alt subseq sum till idx doin op[j] at tht idx
+        ll pos=-1e6,neg=0;
         
-            7 2 3 9 1 6
-            dp[i][0]-->add
-            dp[i][1]-->sub
-            
-        */
+        for(int i=1;i<=n;i++)
+        {
+            ll tpos=max(pos,neg+nums[i-1]);
+            ll tneg=max(neg,pos-nums[i-1]);
+            pos=tpos,neg=tneg;
+        }
         
-        memset(dp,-1LL,sizeof(dp));
-        return helper(nums,0,0);
+        return max(pos,neg);
     }
 };
+
+/*
+    0 1 2 3 4
+    i 4 2 7 7
+    0 0 2 2 3
+*/
