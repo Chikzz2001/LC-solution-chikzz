3e1c55e1f252620586b08b1bfcc182942dce4ee8
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-30 09:25:56 +0530

    Time: 11 ms (86.97%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,4 +1,3 @@
-#define ll long long int
 class Solution {
     int mCount(vector<int>& nums,int t)
     {
@@ -19,12 +18,3 @@ public:
     }
 };
 
-/*
-
-1 0 0 8 6
-0 1 1 7 5
-3*x-1+8-x+6-x=14==>x=13
-
-1 2 9 10
-4 3 4 5
-*/
