c008c90ba3f0c53ddb79cd4dfc68e6e206c039d6
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-30 14:06:12 +0530

    Time: 333 ms (10.20%), Space: 71.6 MB (12.31%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,41 +1,34 @@
-// #define ms_iter multiset<int>::iterator
 class Solution {
 public:
     bool isPossible(vector<int>& nums) 
     {
-       unordered_map<int,multiset<int>>m;
+       unordered_map<int,priority_queue<int>>m;
         for(auto &x:nums)
         {
             if(!m.count(x-1))
             {
-                m[x].insert(1);
+                m[x].push(-1);
             }
             else
             {
-                auto it=m[x-1].begin();
-                int freq=*it;
-                m[x].insert(freq+1);
-                m[x-1].erase(it);
-                //cout<<x-1<<"\n";
+                int freq=m[x-1].top();
+                m[x].push(freq-1);
+                m[x-1].pop();
                 if(m[x-1].size()==0)m.erase(x-1);
             }
         }
         
         for(auto &[x,y]:m)
         {
-            for(auto &z:y)
-            { 
-                //cout<<x<<" "<<z<<"\n";
-                if(z<3)return 0;
+            while(y.size())
+            {
+                if(-y.top()<3)
+                    return 0;
+                y.pop();
             }
         }
         return 1;
     }
 };
 
-/*
-
-
-
-*/
 
