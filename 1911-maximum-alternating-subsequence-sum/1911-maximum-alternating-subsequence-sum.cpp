7deed9c8e93bf2fd89df818384da6d488009fdb0
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-30 11:05:02 +0530

    Time: 291 ms (50.39%), Space: 91.1 MB (74.68%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,28 +1,10 @@
 #define ll long long int
 class Solution {
-//     ll dp[100001][2];
-//     ll helper(vector<int>& nums,int op,int i)
-//     {
-//         if(i==nums.size())
-//             return 0;
-        
-//         if(dp[i][op]!=-1)
-//             return dp[i][op];
-        
-//         ll maxx=helper(nums,op,i+1);
-        
-//         if(op)
-//             maxx=max(maxx,-nums[i]+helper(nums,0,i+1));
-//         else
-//             maxx=max(maxx,nums[i]+helper(nums,1,i+1));
-        
-//         return dp[i][op]=maxx;
-//     }
 public:
     long long maxAlternatingSum(vector<int>& nums) 
     {
         int n=nums.size();
-        //dp[i][j]-->max alt subseq sum till idx doin op[j] at tht idx
+
         ll pos=-1e6,neg=0;
         
         for(int i=1;i<=n;i++)
@@ -35,9 +17,3 @@ public:
         return max(pos,neg);
     }
 };
-
-/*
-    0 1 2 3 4
-    i 4 2 7 7
-    0 0 2 2 3
-*/
