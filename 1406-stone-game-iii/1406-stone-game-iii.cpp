deab47d8bb81806f303379de046ffa70c8b262cb
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-06-30 20:03:39 +0530

    Time: 323 ms (67.46%), Space: 126.9 MB (90.92%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,40 +1,24 @@
 #define ll long long
 class Solution {
-    ll dp[500001];
-    int help(int i,vector<int>& s)
-    {
-        if(i>=s.size())return 0;
-        
-        if(dp[i]!=-1)return dp[i];
-        
-        ll maxx=-1e9;
-        ll take=0;
-        for(int j=i;j<min((int)s.size(),i+3);j++)
-        {
-            take+=s[j];
-            maxx=max(maxx,take-help(j+1,s));
-        }
-        return dp[i]=maxx;
-    }
 public:
     string stoneGameIII(vector<int>& s) {
+       
+        int n=s.size();
+        ll dp[n+1];
         
-        long long sum=0;
-        for(auto &x:s)sum+=x;
-        
-        memset(dp,-1,sizeof(dp));
-        
-        long long diff=help(0,s);
-        //for(int i=0;i<s.size();i++)cout<<dp[i]<<" ";
-        return diff>0?"Alice":diff==0?"Tie":"Bob";
+        //dp[i]-->stores max value that a person can obtain if he has to start from ith idx
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[i]=-1e9;
+            ll take=0;
+            for(int j=i;j<min(n,i+3);j++)
+            {
+                take+=s[j];
+                dp[i]=max(dp[i],take-dp[j+1]);
+            }
+        }
+    
+        return dp[0]>0?"Alice":dp[0]==0?"Tie":"Bob";
     }
 };
-
-/*
- 1-(2-(3-7))
- 1-(2+3-7)
- 1-(2+3+7)
- 1+2-(3-7)
- 1+2-(3+7)
- 1+2+3-7
-*/
