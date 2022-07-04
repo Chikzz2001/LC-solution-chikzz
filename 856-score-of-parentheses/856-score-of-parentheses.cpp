0faf261af2ba8ac7f0e6b484e91b9c50c24c5447
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-04 13:40:07 +0530

    Time: 4 ms (25.14%), Space: 6.3 MB (6.10%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,21 +1,53 @@
 class Solution {
+    map<int,int>cl;
+
+int f(string&str,int s,int e){
+      if(abs(s-e)==1)return 1;
+     if(s>=e)return 0;
+    
+      int end_point=cl[s];
+        
+      if(end_point==e)return 2*f(str,s+1,e-1);
+      
+      return f(str,s,end_point)+f(str,end_point+1,e);  
+}
+
 public:
     int scoreOfParentheses(string s) 
     {
-        int depth=-1;
-        int res=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='(')
-            depth++;
-            else
-            {
-                if(s[i-1]=='(')
-                res+=1<<depth;
-                depth--;
-            }
+        // int depth=-1;
+        // int res=0;
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s[i]=='(')
+        //     depth++;
+        //     else
+        //     {
+        //         if(s[i-1]=='(')
+        //         res+=1<<depth;
+        //         depth--;
+        //     }
+        // }
+        // return res;
+         int n=s.size();
+     
+     if(n==2)return 1;
+
+     stack<int>st;
+
+    for(int i=0;i<n;i++){
+        if(s[i]=='('){
+          st.push(i);
         }
-        return res;
+        else{
+            int open=st.top();
+            st.pop();
+            cl[open]=i;
+         }
+    }
+
+    return f(s,0,n-1);
+
     }
 };
 
