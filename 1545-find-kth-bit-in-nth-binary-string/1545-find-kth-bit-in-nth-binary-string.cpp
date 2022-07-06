8dd4df0202c4f774349d5e9c517b5ec0b82b0c44
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-06 15:57:22 +0530

    Time: 3 ms (79.63%), Space: 5.9 MB (78.90%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    string dp[21]={""};
-    string dp2[21]={""};
-    string f(string s,int n)
+    char f(int n,int k)
     {
-        if(dp2[n]!="")return dp2[n];
+        if(n==1)return '0';
         
-        reverse(s.begin(),s.end());
-        for(auto &x:s)
+//         if(dp[k][n]!='#')
+//             return dp[k][n];
+        
+        int half=1<<(n-1);
+        if(k==half)
+            return '1';
+        
+        if(k<half)
         {
-            x=x=='1'?'0':'1';
+            return f(n-1,k);
         }
-        return dp2[n]=s;
-    }
-    
-    string generate(int n)
-    {
-        if(n==1)return dp[1]="0";
-        if(dp[n]!="")return dp[n];
-        return dp[n]=generate(n-1)+"1"+f(generate(n-1),n-1);
+        else
+        {
+            return f(n-1,2*half-k)=='1'?'0':'1';
+        }
     }
 public:
-    char findKthBit(int n, int k) {
-        
-        string st=generate(n);
-       // cout<<st;
-        return st[k-1];
+    char findKthBit(int n, int k) 
+    {
+        //vector<vector<char>>dp(k+1,vector<char>(n+1,'#'));
+        return f(n,k);
     }
 };
