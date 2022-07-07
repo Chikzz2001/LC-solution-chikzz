b870890e10d14b114550e4468b15d52268d17d79
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-07 11:02:39 +0530

    Time: 3 ms (85.12%), Space: 6.7 MB (58.84%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -23,13 +23,7 @@ public:
                        dp[i][j]=1;
             }
         }
-        
-        // for(int i=0;i<=n;i++)
-        // {
-        //     for(int j=0;j<=m;j++)
-        //         cout<<dp[i][j]<<" ";
-        //     cout<<"\n";
-        // }
+
         return dp[n][m];
     }
 };
