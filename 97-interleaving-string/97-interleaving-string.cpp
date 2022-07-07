2a65251b7b0e039c65e5ba59b412bc9bd137b924
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-07 11:10:11 +0530

    Time: 4 ms (73.94%), Space: 6.3 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -7,23 +7,26 @@ public:
         
         int n=s1.length(),m=s2.length();
         if(n+m!=s3.length())return 0;
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
-        
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++)if(s1[i-1]==s3[i-1])dp[i][0]=dp[i-1][0];
-        for(int i=1;i<=m;i++)if(s2[i-1]==s3[i-1])dp[0][i]=dp[0][i-1];
+        //vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
+        vector<bool>dp(m+1,0);
+        dp[0]=1;
+ 
+        for(int i=1;i<=m;i++)if(s2[i-1]==s3[i-1])dp[i]=dp[i-1];
         
         for(int i=1;i<=n;i++)
         {
+            vector<bool>temp(m+1,0);
+            temp[0]=(s1[i-1]==s3[i-1])&dp[0];
             for(int j=1;j<=m;j++)
             {
-                    if(s1[i-1]==s3[i+j-1]&&dp[i-1][j])
-                       dp[i][j]=1;
-                    if(s2[j-1]==s3[i+j-1]&&dp[i][j-1])
-                       dp[i][j]=1;
+                if(s1[i-1]==s3[i+j-1]&&dp[j])
+                temp[j]=1;
+                if(s2[j-1]==s3[i+j-1]&&temp[j-1])
+                temp[j]=1;
             }
+            dp=temp;
         }
 
-        return dp[n][m];
+        return dp[m];
     }
 };
