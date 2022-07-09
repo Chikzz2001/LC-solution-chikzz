fcffff23fe743417c39d4f2fa63301e66c8237de
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-09 23:41:21 +0530

    Time: 37 ms (55.66%), Space: 12.8 MB (16.29%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumLength(string s) 
+    {
+        int n=s.length();
+        if(n==1)return 1;
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+                return (j-i+1);
+            char x=s[i];
+            while(i<j&&s[i]==x)
+                i++;
+            if(i==j)return 0;
+            while(i<j&&s[j]==x)
+                j--;
+            if(i==j)return 1;
+        }
+        return 0;
+    }
+};
+
+/*
+
+"bbbbbbbbbbbbbbbbbbbbbbbbbbb a bbbbbbbbbbbbbbb cc b c b c b cc bb a bbb"
+"c a b aa b a c"
+*/
