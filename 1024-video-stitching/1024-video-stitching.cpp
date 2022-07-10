15a79a2a77c8ba3c2d84963e45c835aafb726017
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-10 14:48:22 +0530

    Time: 7 ms (36.36%), Space: 7.8 MB (45.45%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int videoStitching(vector<vector<int>>& clips, int time) 
+    {
+        sort(clips.begin(),clips.end());
+        if(clips[0][0]!=0)return -1;
+        int n=clips.size();
+        int dp[n+1];
+        int i=0;
+        int res=n+2;
+        for(;i<n&&clips[i][0]==0;i++)
+        {dp[i]=1;if(clips[i][1]>=time)return 1;}
+
+        for(;i<n;i++)
+        {
+            dp[i]=i+2;
+            for(int j=i-1;j>=0;j--)
+            {
+                if(clips[j][1]>=clips[i][0])
+                {
+                    dp[i]=min(dp[i],1+dp[j]);
+                }
+            }
+            
+            if(dp[i]!=i+2&&clips[i][1]>=time)
+                res=min(res,dp[i]);
+        }
+        
+        return res==n+2?-1:res; 
+    }
+};
