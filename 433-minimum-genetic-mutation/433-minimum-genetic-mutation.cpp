ebfa45852ea1b71cd82d541fb1c2a440902947fa
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-16 10:08:18 +0530

    Time: 4 ms (28.74%), Space: 6.5 MB (96.71%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) 
+    {
+        set<string>genes;
+        
+        for(auto &g:bank)
+            genes.insert(g);
+        
+        if(!genes.count(end))return -1;
+        
+        char gene[4]={'A','C','G','T'};
+        
+        int n=start.length();
+        
+        queue<string>Q;
+        Q.push(start);
+        
+        set<string>vis;
+        vis.insert(start);
+        
+        int level=0;
+        while(!Q.empty())
+        {
+            int size=Q.size();
+            while(size--)
+            {
+                string curr=Q.front();
+                Q.pop();
+                
+                if(curr==end)return level;
+                
+                for(int i=0;i<n;i++)
+                {
+                    char ch=curr[i];
+                    for(int j=0;j<4;j++)
+                    {
+                        if(ch!=gene[j])curr[i]=gene[j];
+                        if(!vis.count(curr))
+                        {
+                            
+                            if(genes.count(curr))
+                            {
+                                if(curr==end)return level+1;
+                                Q.push(curr);
+                                vis.insert(curr);
+                            }
+                        }
+                    }
+                    curr[i]=ch;
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+};
