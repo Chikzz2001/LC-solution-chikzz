76e731c762ac99e43731f517199dfe5498582238
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-16 13:04:18 +0530

    Time: 281 ms (16.52%), Space: 111.6 MB (49.48%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -15,37 +15,43 @@ public:
             facedown[bottoms[i]]++;
         }
         
-        vector<int>possibilities;
+        int maxup=-1,maxdown=-1;
         for(int i=1;i<7;i++)
         {
             if(faceup[i]+facedown[i]-common[i]==n)
-                possibilities.push_back(i);
+            {
+                maxup=max(maxup,faceup[i]);
+                maxdown=max(maxdown,facedown[i]);
+            }
         }
         
-        if(!possibilities.size())
-            return -1;
+        if(maxup==-1)return -1;
+        return min(n-maxup,n-maxdown);
+            
+//         if(!possibilities.size())
+//             return -1;
         
-        int minn=1e9;
-        for(auto &x:possibilities)
-        {
-            // int count1=0,count2=0;
-            // for(int i=0;i<n;i++)
-            // {
-            //     if(tops[i]==x&&bottoms[i]==x)continue;
-            //   if(tops[i]==x)  
-            //   {
-            //      count1++; 
-            //   }
-            //   else if(bottoms[i]==x)
-            //   {
-            //     count2++;
-            //   }
-            // } 
-            // minn=min({minn,count1,count2});
-            //cout<<faceup[x]<<" "<<faceup[x]<<"\n";
-            minn=min({minn,n-faceup[x],n-facedown[x]});
-        }
+//         int minn=1e9;
+//         for(auto &x:possibilities)
+//         {
+//             // int count1=0,count2=0;
+//             // for(int i=0;i<n;i++)
+//             // {
+//             //     if(tops[i]==x&&bottoms[i]==x)continue;
+//             //   if(tops[i]==x)  
+//             //   {
+//             //      count1++; 
+//             //   }
+//             //   else if(bottoms[i]==x)
+//             //   {
+//             //     count2++;
+//             //   }
+//             // } 
+//             // minn=min({minn,count1,count2});
+//             //cout<<faceup[x]<<" "<<faceup[x]<<"\n";
+//             minn=min({minn,n-faceup[x],n-facedown[x]});
+//         }
         
-        return minn;
+//         return minn;
     }
 };
