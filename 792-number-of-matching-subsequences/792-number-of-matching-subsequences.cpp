416bd839c74a8fcb2200c87bfe8965172f440d27
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-07-20 09:24:57 +0530

    Time: 1445 ms (5.57%), Space: 169.9 MB (29.73%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,35 +1,37 @@
 class Solution {
-    unordered_map<string,bool>istrue;
-    bool match(string &s,string t)
-    {
-        int sp=0,tp=0;
-        while(sp<s.length()&&tp<t.length())
-        {
-            if(s[sp]==t[tp])
-                tp++;
-            sp++;
-        }
-        return tp==t.length();
-    }
 public:
     int numMatchingSubseq(string s, vector<string>& words) 
     {
-        int count=0;
-        for(auto &w:words)
+        vector<set<int>>v(26);
+        int n=words.size();
+        vector<int>len(n,0);
+        for(int i=0;i<n;i++)
         {
-            if(istrue.count(w))
-            {
-                count+=istrue[w];
-                continue;
-            }
-            if(match(s,w))
-            {
-                istrue[w]=1;
-                count++;
-            }
-            else
-                istrue[w]=0;
+            v[words[i][0]-'a'].insert(i);
         }
-        return count;
+        
+        int res=0;
+        for(int i=0;i<s.length();i++)
+        {
+            set<int>temp=v[s[i]-'a'];
+            for(auto &it:temp)
+            {
+                len[it]++;
+                if(len[it]==words[it].length())
+                {
+                    v[words[it][len[it]-1]-'a'].erase(it);
+                    res++;
+                }
+                else
+                {
+                    if(words[it][len[it]]!=words[it][len[it]-1])
+                    {
+                        v[words[it][len[it]-1]-'a'].erase(it);
+                        v[words[it][len[it]]-'a'].insert(it);
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
