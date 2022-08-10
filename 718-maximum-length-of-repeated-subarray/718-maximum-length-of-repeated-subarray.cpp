cead5c6439a85acab325d18f2e4e342543935747
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-08-10 09:50:08 +0530

    Time: 616 ms (25.55%), Space: 11.7 MB (97.58%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//         int n=nums1.size(),m=nums2.size();
+//         int dp[n][m];
+//         int ma=INT_MIN;
+//         for(int i=0;i<n;i++){
+//             dp[i][0]=(nums1[i]==nums2[0])?1:0;
+//             ma=max(ma,dp[i][0]);} 
+//         for(int i=0;i<m;i++)
+//         {dp[0][i]=(nums1[0]==nums2[i])?1:0;
+//         ma=max(ma,dp[0][i]);}
+//         for(int i=1;i<n;i++)
+//         {
+//             for(int j=1;j<m;j++)
+//             {
+//                 if(nums1[i]==nums2[j])dp[i][j]=1+dp[i-1][j-1];
+//                 else
+//                     dp[i][j]=0;
+//                 ma=max(ma,dp[i][j]);
+//             }
+//         }
+//         return ma;
+//     }
+// };
+
 class Solution {
     int maxsublen_of_first_in_second(vector<int>& nums1,vector<int>& nums2)
     {
@@ -30,10 +56,3 @@ public:
         return max(maxsublen_of_first_in_second(nums1,nums2),maxsublen_of_first_in_second(nums2,nums1));
     }
 };
-
-/*
-
-[0,0,0,0,0,1,1,1,0,1,0,1,1,0,0,0,1,0,0,1,1,0,1,0,1,1,1,0,0,1,0,1,1,0,1,0,1,1,1,1,1,1,1,0,0,0,0,0,1,0,1,1,0,0,0,1,1,1,1,1,0,1,1,1,0,0,1,1,0,1,1,1,0,1,1,1,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,1,1,0,1,0,0,0,1]
-[1,0,1,1,0,1,1,1,0,0,0,0,0,1,0,1,1,0,0,0,1,1,1,1,1,0,1,1,1,0,0,1,1,0,1,1,1,0,1,1,1,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,0,1,1,0,1,0,0,0,1,0,1,0,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,1,0,0,0,1,1,1,1,1,1,1,1,0]
-
-*/
