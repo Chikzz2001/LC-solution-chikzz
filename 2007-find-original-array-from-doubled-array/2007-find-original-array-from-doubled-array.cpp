80618a1352340eb9a78969324af9e312679ab896
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2022-09-15 06:57:31 +0530

    Time: 833 ms (30.04%), Space: 143.9 MB (58.97%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -14,9 +14,11 @@ public:
         {
             if(!freq[x])continue;
             freq[x]--;
-            if(!freq[2*x])
+            if(!freq[x])freq.erase(x);
+            if(!freq.count(2*x))
             return {};
             freq[2*x]--;
+            if(!freq[2*x])freq.erase(2*x);
             res.push_back(x);
         }
         return res;
@@ -26,7 +28,4 @@ public:
 /*
 
 1 2 3 4 6 8
-
-1 1 2 4
-
 */
