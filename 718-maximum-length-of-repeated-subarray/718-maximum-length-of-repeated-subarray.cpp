d3ded3f01e8442639d7ac8509d6fdaa90c798d47
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2022-09-20 06:51:28 +0530

    Time: 364 ms (74.06%), Space: 16.1 MB (81.84%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,58 +1,25 @@
-// class Solution {
-// public:
-//     int findLength(vector<int>& nums1, vector<int>& nums2) {
-//         int n=nums1.size(),m=nums2.size();
-//         int dp[n][m];
-//         int ma=INT_MIN;
-//         for(int i=0;i<n;i++){
-//             dp[i][0]=(nums1[i]==nums2[0])?1:0;
-//             ma=max(ma,dp[i][0]);} 
-//         for(int i=0;i<m;i++)
-//         {dp[0][i]=(nums1[0]==nums2[i])?1:0;
-//         ma=max(ma,dp[0][i]);}
-//         for(int i=1;i<n;i++)
-//         {
-//             for(int j=1;j<m;j++)
-//             {
-//                 if(nums1[i]==nums2[j])dp[i][j]=1+dp[i-1][j-1];
-//                 else
-//                     dp[i][j]=0;
-//                 ma=max(ma,dp[i][j]);
-//             }
-//         }
-//         return ma;
-//     }
-// };
-
 class Solution {
-    int maxsublen_of_first_in_second(vector<int>& nums1,vector<int>& nums2)
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) 
     {
         int n=nums1.size(),m=nums2.size();
+        int dp[n+1][m+1];
+        
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int i=0;i<=m;i++)dp[0][i]=0;
+        
         int res=0;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<m;)
+            for(int j=1;j<=m;j++)
             {
-                if(nums1[i]==nums2[j])
-                {
-                    int k=i;
-                    int len=0;
-                    while(j<m&&k<n&&nums1[k]==nums2[j])
-                        j++,k++,len++;
-                    if(len>res)
-                    {
-                        res=max(res,len);
-                    }
-                    
-                }
+                if(nums1[i-1]==nums2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
                 else
-                    j++;
+                    dp[i][j]=0;
+                res=max(res,dp[i][j]);
             }
         }
         return res;
-    }
-public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        return max(maxsublen_of_first_in_second(nums1,nums2),maxsublen_of_first_in_second(nums2,nums1));
     }
 };
