bb42754e97be6681d9f764ce9c21e4e722f9bb80
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-02-20 00:59:50 +0530

    Time: 170 ms (83.64%), Space: 109.4 MB (40.96%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -9,32 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#define ff first
+#define ss second
 class Solution {
-    int res=0;
-    struct node
-    {
-        int minn,maxx,sum;
-    };
-    node helper(TreeNode* root)
-    {
-        if(!root)
-            return {INT_MAX,INT_MIN,0};
-        
-        auto [lminn,lmaxx,lsum]=helper(root->left);
-        auto [rminn,rmaxx,rsum]=helper(root->right);
-        
-        if(root->val>lmaxx&&root->val<rminn)
-        {
-            res=max(res,lsum+rsum+root->val);
-            return {min({lminn,rminn,root->val}),max({lmaxx,rmaxx,root->val}),lsum+rsum+root->val};
+    int maxx=0;
+    pair<int,pair<int,int>> helper(TreeNode* root) {
+        if(!root)return {0,{INT_MIN,INT_MAX}};
+        auto L=helper(root->left);
+        auto R=helper(root->right);
+        bool ok=1;
+        if(root->val<=L.ss.ff)ok=0;
+        if(root->val>=R.ss.ss)ok=0;
+        if(ok) {
+            maxx=max(maxx,root->val+L.ff+R.ff);
         }
-        
-        res=max({res,lsum,rsum});
-        return {INT_MIN,INT_MAX,0};
+        if(ok)
+        return {L.ff+R.ff+root->val,{max({L.ss.ff,R.ss.ff,root->val}),min({L.ss.ss,R.ss.ss,root->val})}};
+        return {0,{INT_MAX,INT_MIN}};
     }
 public:
     int maxSumBST(TreeNode* root) {
         helper(root);
-        return res;
+        return maxx;
     }
 };
