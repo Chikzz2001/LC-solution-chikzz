d4f9fcba0d7b0198eb7c342a492c85e92c1e7f31
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-02-22 16:13:50 +0530

    Time: 112 ms (8.68%), Space: 26 MB (77.07%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,45 +1,24 @@
 class Solution {
-    bool valid(vector<int>& weights,int days,int max_weight)
-    {
-        int count_days=0;
-        int wt=0;
-        for(int i=0;i<weights.size();i++)
-        {
-            if(wt+weights[i]>max_weight)
-            {
-                count_days++;
-                wt=weights[i];
-            }
-            else
-                wt+=weights[i];
-            if(count_days>days)return 0;
-        }
-        count_days++;
-        return count_days<=days;
-    }
 public:
-    int shipWithinDays(vector<int>& weights, int days) 
-    {
-        int max_weight=0,total_weight=0;
-        for(auto &w:weights)total_weight+=w,max_weight=max(max_weight,w);
+    int shipWithinDays(vector<int>& wt, int d) {
+        int l=0,r=1e9;
         
-        int lo=max_weight;
-        int hi=total_weight;
-        
-        int res;
-        while(lo<=hi)
-        {
-            int mid=lo+(hi-lo)/2;
-            if(valid(weights,days,mid))
-            {
-                res=mid;
-                hi=mid-1;
+        while((r-l)>1) {
+            int m=l+r>>1;
+            int D=1,S=0;
+            bool ok=1;
+            for(int i=0;i<wt.size();i++) {
+                if(wt[i]>m){ok=0;break;}
+                if(S+wt[i]>m) {
+                    D++;
+                    S=wt[i];
+                }
+                else S+=wt[i];
             }
-            else
-            {
-                lo=mid+1;
-            }
+            if(ok&&D<=d)r=m;
+            else l=m;
         }
-        return res;
+        
+        return r;
     }
 };
