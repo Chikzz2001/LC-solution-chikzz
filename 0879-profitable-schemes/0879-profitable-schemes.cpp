ce0c4a34f98282da6a9677b93810baf9e60db70b
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-04-21 08:36:07 +0530

    Time: 217 ms (47.17%), Space: 12.7 MB (72.64%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -3,15 +3,15 @@ class Solution {
     const int M=1e9+7;
     int dp[101][101][101];
     int N,PM;
-    ll helper(vector<int>& group, vector<int>& profit, int n,int minProfit,int i,int P,int ppl,int c) {
+    ll helper(vector<int>& group, vector<int>& profit, int n,int minProfit,int i,int P,int ppl) {
         if(i==profit.size()) {
             if(P>=minProfit&&ppl<=n)return 1;
             return 0;
         }
         if(ppl>n)return 0;
         if(dp[i][min(P,minProfit)][ppl]!=-1)return dp[i][min(P,minProfit)][ppl];
-        ll op1=helper(group,profit,n,minProfit,i+1,P+profit[i],ppl+group[i],c+1)%M;
-        ll op2=helper(group,profit,n,minProfit,i+1,P,ppl,c)%M;
+        ll op1=helper(group,profit,n,minProfit,i+1,P+profit[i],ppl+group[i])%M;
+        ll op2=helper(group,profit,n,minProfit,i+1,P,ppl)%M;
         
         return dp[i][min(P,minProfit)][ppl]=(op1+op2)%M;
     }
@@ -19,6 +19,6 @@ public:
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
         N=profit.size();
         memset(dp,-1,sizeof(dp));
-        return helper(group,profit,n,minProfit,0,0,0,0);
+        return helper(group,profit,n,minProfit,0,0,0);
     }
 };
