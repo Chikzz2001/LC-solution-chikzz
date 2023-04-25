76d777513e1aaffb5f7e19be920982d624840906
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-04-25 22:57:41 +0530

    Time: 210 ms (79.45%), Space: 26.9 MB (60.08%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -1,32 +1,23 @@
 #define ll long long
 class Solution {
-    map<pair<int,int>,ll>mp;
     ll dp[201][201];
-    ll helper(int i_len,int j_len) {
-        if(i_len<=0||j_len<=0) {
-            return 0;
-        }
-        
-        if(dp[i_len][j_len]!=-1)return dp[i_len][j_len];
-        ll maxx=mp.count({i_len,j_len})?mp[{i_len,j_len}]:0;
-        
-        for(int i=1;i<i_len;i++) {
-            maxx=max(maxx,helper(i,j_len)+helper(i_len-i,j_len));
-        }
-        for(int j=1;j<j_len;j++) {
-            maxx=max(maxx,helper(i_len,j)+helper(i_len,j_len-j));
-        }
-        return dp[i_len][j_len]=maxx;
-    }
 public:
-    long long sellingWood(int m, int n, vector<vector<int>>& prices) {
-        swap(n,m);
-        
-        memset(dp,-1,sizeof(dp));
+    long long sellingWood(int n, int m, vector<vector<int>>& prices) {
         for(auto p:prices) {
-            mp[{p[0],p[1]}]=p[2];
+            dp[p[0]][p[1]]=p[2];
         }
         
-        return helper(n,m);
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                for(int k=1;k<=i;k++) {
+                    dp[i][j]=max(dp[i][j],dp[k][j]+dp[i-k][j]);
+                }
+                for(int k=1;k<=j;k++) {
+                    dp[i][j]=max(dp[i][j],dp[i][k]+dp[i][j-k]);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
