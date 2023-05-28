fcec64ad0c772c357d9decee074d22b9f5209383
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-05-28 16:45:12 +0530

    Time: 1550 ms (27.27%), Space: 88.6 MB (81.82%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
@@ -0,0 +1,82 @@
+class Solution {
+    vector<int> SieveOfEratosthenes(int n)
+    {
+        bool prime[n + 1];
+        memset(prime, true, sizeof(prime));
+
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+
+        vector<int>P;
+        for (int p = 2; p <= n; p++)
+            if (prime[p])
+                P.push_back(p);
+
+            return P;
+    }
+    class DSU {
+        public:
+        vector<int>par,size;
+        DSU(int n) {
+            par=vector<int>(n);
+            size=vector<int>(n,1);
+            for(int i=0;i<n;i++)par[i]=i;
+        }
+        int find(int x) {
+            if(x==par[x])return x;
+            return par[x]=find(par[x]);
+        }
+        void join(int a,int b) {
+            int pa=find(a);
+            int pb=find(b);
+            if(pa==pb)return;
+            if(size[pa]<size[pb]) {
+                par[pa]=par[pb];
+                size[pb]+=size[pa];
+            }
+            else {
+                par[pb]=par[pa];
+                size[pa]+=size[pb];
+            }
+        }
+    };
+public:
+    bool canTraverseAllPairs(vector<int>& nums) {
+         int n=nums.size();
+        if(n==1)return 1;
+        int maxx=*max_element(nums.begin(),nums.end());
+        vector<int>P=SieveOfEratosthenes(317);
+       
+        DSU dsu(n);
+        map<int,int>id;
+        for(int i=0;i<n;i++) {
+            if(nums[i]==1)return 0;
+            for(int j=2;j*j<=nums[i];j++) {
+                if(nums[i]%j==0) {
+                    if(id.count(j)) {
+                        dsu.join(id[j],i);
+                    }
+                    else {
+                        id[j]=i;
+                    }
+                    while(nums[i]%j==0) {
+                        nums[i]/=j;
+                    }
+                }
+            }
+            if(nums[i]>1) {
+                        if(id.count(nums[i])) {
+                            dsu.join(id[nums[i]],i);
+                        }
+                        else {
+                            id[nums[i]]=i;
+                        }
+                    }
+        }
+        return dsu.size[dsu.find(0)]==n;
+    }
+};
