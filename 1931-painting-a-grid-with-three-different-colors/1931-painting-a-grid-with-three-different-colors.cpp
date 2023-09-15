9bf7f088de3929f218eb87474b5cac9b91407f06
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-09-15 10:08:42 +0530

    Time: 517 ms (25.17%), Space: 10.6 MB (65.99%) - LeetHub

---
+++ 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    vector<vector<int>>g;
+    vector<int>t;
+    vector<vector<int>>dp;
+    const int M=1e9+7;
+    
+    void generate(int n) {
+        if(t.size()==n) {
+            g.push_back(t);
+            return;
+        }
+        
+        for(int i=0;i<=2;i++) {
+            if(!t.size()||t.back()!=i) {
+                t.push_back(i);
+                generate(n);
+                t.pop_back();
+            }
+        }
+    }
+    
+    int helper(int i,int m,int pre) {
+        if(i==m)return 1;
+        
+        if(dp[i][pre]!=-1)return dp[i][pre];
+        
+        int res=0;
+        for(int j=0;j<g.size();j++) {
+            bool match=1;
+            for(int k=0;k<g[j].size();k++) {
+                if(g[pre][k]==g[j][k]){match=0;break;}
+            }
+            if(match) {
+                res=(res%M+helper(i+1,m,j)%M)%M;
+            }
+        }
+        return dp[i][pre]=res;
+    }
+public:
+    int colorTheGrid(int n, int m) {
+        generate(n);
+        
+        int sz=g.size();
+        dp=vector<vector<int>>(m,vector<int>(sz,-1));
+        int res=0;
+        for(int i=0;i<sz;i++) {
+            res=(res%M+helper(1,m,i)%M)%M;
+        }
+        return res;
+    }
+};
