827878861464cd8966358dae5afbbc1e7597cb22
Dibyabrata Panja <88380455+Chikzz2001@users.noreply.github.com>
2023-10-01 14:08:38 +0530

    Time: 611 ms (28.57%), Space: 133.3 MB (42.86%) - LeetHub

--- 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.cpp
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.cpp
@@ -1,15 +1,19 @@
+//FOR INFINITE APPEND OF STRING/ARRAY MAKE IT DOUBLE AND CHECK AS IN A DOUBLE ARRAY WE GET ALL 
+//POSSIBLE SUBARRAYS WE CAN GET IN AN INFINITE CONCATENATION OF THE ARRAY
+
 #define ll long long
 class Solution {
 public:
     int minSizeSubarray(vector<int>& nums, int target) {
-        long long s=0;
+        ll s=0;
         for(int x:nums)s+=1ll*x;
+        
         int tot=target/s;
-        int tar=target%s;
+        
         vector<int>v=nums;
         for(int x:nums)v.push_back(x);
-        // cout<<s<<" ";
         
+        //checking if target%s present with 2xnums
         map<ll,int>m;
         ll pre=0;
         m[0]=-1;
@@ -17,32 +21,30 @@ public:
         int res=1e9;
        for(int i=0;i<v.size();i++) {
            pre+=v[i];
-           //pre-tar
            if(m.count(pre-target)) {
                r=min(r,i-m[pre-target]);
            }
            m[pre]=i;
        }
-        // cout<<r<<" ";
         if(r!=1e9)res=r;
-        // cout<<target%s<<"\n";
-        // cout<<tot;
+    
+        //so we also can have the pattern as nums(suf) nums nums nums.....(tot times) nums(pre)
+        //so search max subarray with s-target%s sum and n-r would be my suf+pre  
         target=s-target%s;
-        // cout<<target;
+
         m.clear();
         m[0]=-1;
         r=-1;
         pre=0;
         for(int i=0;i<v.size();i++) {
            pre+=v[i];
-           //pre-tar
            if(m.count(pre-target)) {
                r=max(r,i-m[pre-target]);
            }
             if(!m.count(pre))
             m[pre]=i;
        }
-        // cout<<r;
+    
         int n=nums.size();
         if(r!=-1) {
             res=min(res,(tot*n)+(n-r));
@@ -51,8 +53,3 @@ public:
         return res==1e9?-1:res;
     }
 };
-
-//[2,1,5,7,7,1,6,3]
-//39
-// [1,2,2,2,1,2,1,2,1,2,1]
-// 83
